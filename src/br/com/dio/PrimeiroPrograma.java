d99d6314fab114ea7930d775d3d2b03d4099b76a
wberaldo92 <wellington.beraldo92@gmail.com>
2022-10-23 15:44:11 -0300

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 3;
+        int b = 4;
+        System.out.println("Hello people " + (a+b));*/
+    }
+}
